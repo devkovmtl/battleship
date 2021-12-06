6195cdce54dd42449249bc54582467c3621350ae
Mikael <dev.kov.mtl@gmail.com>
2021-12-06 14:40:48 -0500

    Webpack project setup with jest tested createComponent

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,5 @@
 import './index.css'
-
-function createComponent(
-  tagName: string,
-  id?: string,
-  classes?: Array<string>,
-  content?: string | HTMLElement
-): HTMLElement {
-  const element = document.createElement(tagName)
-
-  if (id) {
-    element.setAttribute('id', id)
-  }
-
-  if (classes && classes.length) {
-    element.classList.add(...classes)
-  }
-
-  if (content && typeof content === 'string') {
-    element.textContent = `${content}`
-  }
-
-  if (content && content instanceof HTMLElement) {
-    element.appendChild(content)
-  }
-
-  return element
-}
+import createComponent from './createComponent'
 
 const root = document.querySelector('#root')
 
